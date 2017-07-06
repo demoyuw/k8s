0374f54cf11974cbf7728d6ae315a55df3b423c8
YuWei Chou <demoyuw@gmail.com>
2017-07-06 16:07:32 +0800

    Output message write to the log file

--- ubunu16installk8s_master.sh
+++ ubunu16installk8s_master.sh
@@ -2,18 +2,37 @@
 # Ubuntu 16.04.2 LTS install k8s
 # OS hostname need all lower case
 
-apt-get update && apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+
+############################################################################
+# Check root privilege: Make sure only root can run our script
+if [ $EUID -ne 0 ] ; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+LOGFILE=$HOME/ubuntu16installk8s_master.log
+touch $LOGFILE
+
+
+# apt-get install & upgrade
+echo "========================================================================================"  >> $LOGFILE 
+echo "apt-get install & upgrade" >> $LOGFILE
+apt-get update >> $LOGFILE
+apt-get install -y apt-transport-https >> $LOGFILE
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - >> $LOGFILE
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list  
 deb http://apt.kubernetes.io/ kubernetes-xenial main 
 EOF
-apt-get update
-apt-get install -y kubelet kubeadm docker.io
-apt-get update && apt-get upgrade -y
+apt-get update  >> $LOGFILE
+apt-get install -y kubelet kubeadm docker.io  >> $LOGFILE
+apt-get update -y  >> $LOGFILE
+apt-get upgrade -y  >> $LOGFILE
 
 # Create /etc/cni/net.d/10-weave.conf setting 
-mkdir /etc/cni
-mkdir /etc/cni/net.d
+echo "========================================================================================"  >> $LOGFILE 
+echo "add /etc/cni/net.d/10-weave.conf" >> $LOGFILE
+mkdir /etc/cni  >> $LOGFILE 
+mkdir /etc/cni/net.d  >> $LOGFILE 
 cat <<EOF >/etc/cni/net.d/10-weave.conf  
 {
     "name": "weave",
@@ -21,12 +40,20 @@ cat <<EOF >/etc/cni/net.d/10-weave.conf
 }
 EOF
 
+
+
 # kubernetes initialization
-kubeinit
+echo "========================================================================================"  >> $LOGFILE 
+echo "kubernetes initialization" >> $LOGFILE 
+kubeadm init  >> $LOGFILE
 
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-KUBECONFIG=$HOME/admin.conf
+echo "========================================================================================"  >> $LOGFILE 
+echo "export KUBECONFIG" >> $LOGFILE 
+mkdir -p $HOME/.kube  >> $LOGFILE 
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config  >> $LOGFILE 
+sudo chown $(id -u):$(id -g) $HOME/.kube/config  >> $LOGFILE 
+export KUBECONFIG=$HOME/admin.conf
 
-kubectl get nodes
+echo "========================================================================================"  >> $LOGFILE 
+echo "kubectl get nodes" >> $LOGFILE 
+kubectl get nodes  >> $LOGFILE 
--- ubunu16installk8s_slave.sh
+++ ubunu16installk8s_slave.sh
@@ -3,18 +3,36 @@
 # OS hostname need all lower case
 
 
-apt-get update && apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+############################################################################
+# Check root privilege: Make sure only root can run our script
+if [ $EUID -ne 0 ] ; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+LOGFILE=$HOME/ubuntu16installk8s_slave.log
+touch $LOGFILE
+
+
+# apt-get install & upgrade
+echo "========================================================================================"  >> $LOGFILE 
+echo "apt-get install & upgrade" >> $LOGFILE
+apt-get update >> $LOGFILE
+apt-get install -y apt-transport-https >> $LOGFILE
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - >> $LOGFILE
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list  
 deb http://apt.kubernetes.io/ kubernetes-xenial main 
 EOF
-apt-get update
-apt-get install -y kubelet kubeadm docker.io
-apt-get update && apt-get upgrade -y
+apt-get update  >> $LOGFILE
+apt-get install -y kubelet kubeadm docker.io  >> $LOGFILE
+apt-get update -y  >> $LOGFILE
+apt-get upgrade -y  >> $LOGFILE
 
 # Create /etc/cni/net.d/10-weave.conf setting 
-mkdir /etc/cni
-mkdir /etc/cni/net.d
+echo "========================================================================================"  >> $LOGFILE 
+echo "add /etc/cni/net.d/10-weave.conf" >> $LOGFILE
+mkdir /etc/cni  >> $LOGFILE 
+mkdir /etc/cni/net.d  >> $LOGFILE 
 cat <<EOF >/etc/cni/net.d/10-weave.conf  
 {
     "name": "weave",
