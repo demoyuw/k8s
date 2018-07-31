dd97746ca9d1958b6dd01c9e4884748a5dbc3642
YuWei Chou <demoyuw@gmail.com>
2018-08-01 01:06:52 +0800

    ubuntu16 deploy rancher.

---
+++ rancher/ubunu16_deploy_rancher.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+WORK_DIR=/home/ubuntu
+LOGFILE=$WORK_DIR/log/rancher_install.log
+touch $LOGFILE
+
+apt-get update &>> $LOGFILE
+apt-get install     apt-transport-https     ca-certificates     curl     software-properties-common &>> $LOGFILE
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable" &>> $LOGFILE
+apt-get update &>> $LOGFILE 
+apt-get install docker-ce=17.03.2~ce-0~ubuntu-xenial -y &>> $LOGFILE
+
+mkdir $WORK_DIR/rancher &>> $LOGFILE
+chmod 755 $WORK_DIR/rancher &>> $LOGFILE
+docker run -d --restart=unless-stopped -p 80:80 -p 443:443 -v $WORK_DIR/rancher:/var/lib/rancher rancher/rancher &>> $LOGFILE
