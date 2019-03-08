2b79d99476bae1b26deb4000910c71e7553f3789
YuWei Chou <demoyuw@gmail.com>
2019-03-09 07:17:28 +0800

    add annotations and nodeSelector

--- for-create-vm.sh
+++ for-create-vm.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-for ((i=37; i<=38; i++))
+for ((i=13; i<=18; i++))
 do
-    gcloud beta compute --project=demoyuw instances create k8s-20180805-$i --zone=asia-east1-b --machine-type=n1-standard-1 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=5300134502-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --tags=http-server,https-server --image=ubuntu-1604-xenial-v20180802 --image-project=ubuntu-os-cloud --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=k8s-20180805-$i
+    gcloud beta compute --project=k8s-class2 instances create k8s-20190309-$i --zone=us-west1-b --machine-type=n1-standard-1 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=1070599203664-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --tags=http-server,https-server --image=ubuntu-1604-xenial-v20190306 --image-project=ubuntu-os-cloud --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=k8s-20190309-$i
 done
