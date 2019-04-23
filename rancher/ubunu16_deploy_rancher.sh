4ada097c779e678b4862b29fcd4bfa33112cea3c
yelban <yelban@gmail.com>
2019-04-23 10:36:21 +0800

    universe user name

--- rancher/ubunu16_deploy_rancher.sh
+++ rancher/ubunu16_deploy_rancher.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-WORK_DIR=/home/demoyuw
+WORK_DIR=/home/$USER
 LOGFILE=$WORK_DIR/log/rancher_install.log
-mkdir $WORK_DIR/log
+mkdir -p $WORK_DIR/log
 touch $LOGFILE
 
 apt-get update &>> $LOGFILE
@@ -11,9 +11,9 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable" &>> $LOGFILE
-apt-get update &>> $LOGFILE 
+apt-get update &>> $LOGFILE
 apt-get install docker-ce=17.03.2~ce-0~ubuntu-xenial -y &>> $LOGFILE
 
-mkdir $WORK_DIR/rancher &>> $LOGFILE
+mkdir -p $WORK_DIR/rancher &>> $LOGFILE
 chmod 755 $WORK_DIR/rancher &>> $LOGFILE
 docker run -d --restart=unless-stopped -p 80:80 -p 443:443 -v $WORK_DIR/rancher:/var/lib/rancher rancher/rancher &>> $LOGFILE
