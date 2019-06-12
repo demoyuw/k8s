5dabf7cbdd10cfa0c70e0535d92e68a5a6bef364
YuWei Chou <demoyuw@gmail.com>
2019-06-12 20:24:52 +0800

    update

--- rancher/ubunu16_install_docker.sh
+++ rancher/ubunu16_install_docker.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-WORK_DIR=/home/$USER
+WORK_DIR=/home/demoyuw
 LOGFILE=$WORK_DIR/log/docker_install.log
 mkdir $WORK_DIR/log
 touch $LOGFILE
 
-apt-get update &>> $LOGFILE
-apt-get install     apt-transport-https     ca-certificates     curl     software-properties-common &>> $LOGFILE
+apt-get update -y &>> $LOGFILE
+apt-get install     apt-transport-https     ca-certificates     curl     software-properties-common -y &>> $LOGFILE
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable" &>> $LOGFILE
-apt-get update &>> $LOGFILE 
+apt-get update -y &>> $LOGFILE 
 apt-get install docker-ce=17.03.2~ce-0~ubuntu-xenial -y &>> $LOGFILE
