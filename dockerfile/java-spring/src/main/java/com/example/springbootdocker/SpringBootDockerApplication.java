4396292fabeff2bb279e21d86fdc294a8cd58ae4
YuWei Chou <demoyuw@gmail.com>
2021-08-09 13:41:06 +0800

    dockerfile add java spring

---
+++ dockerfile/java-spring/src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+
+}
