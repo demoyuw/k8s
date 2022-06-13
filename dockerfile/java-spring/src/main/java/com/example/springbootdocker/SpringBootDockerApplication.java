3760622c3e0f31e08dc1675fb839d1782ef03183
YuWei Chou <demoyuw@gmail.com>
2022-06-14 06:51:01 +0800

    Update java spring

--- dockerfile/java-spring/src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
+++ dockerfile/java-spring/src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
@@ -2,12 +2,19 @@ package com.example.springbootdocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootDockerApplication {
+  @RequestMapping("/")
+  public String home() {
+    return "Hello Docker World";
+  }
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDockerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootDockerApplication.class, args);
+  }
 
 }
