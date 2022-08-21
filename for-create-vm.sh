be9b2771ba2b9d82616eb12f4f6c80fd12e6f8e6
YuWei Chou <demoyuw@gmail.com>
2022-08-21 12:34:51 +0800

    update rancher

--- for-create-vm.sh
+++ for-create-vm.sh
@@ -1,11 +1,11 @@
-#! /bin/bash
+#!/bin/bash
 
-for ((i=21; i<=22; i++))
+for ((i=33; i<=35; i++))
 do
-    gcloud compute --project=devops-cicd-3 instances create k8s-20210107-$i \
-    --zone=us-central1-a --machine-type=n1-standard-1 --subnet=default --network-tier=PREMIUM \
-    --maintenance-policy=MIGRATE --service-account=801863942905-compute@developer.gserviceaccount.com \
+    gcloud compute --project=abackend15-pj3 instances create iisi-k8s-vm2-$i \
+    --zone=us-east1-b --machine-type=e2-medium --subnet=default --network-tier=PREMIUM \
+    --maintenance-policy=MIGRATE --service-account=224189083723-compute@developer.gserviceaccount.com \
     --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
-    --tags=http-server,https-server --image=ubuntu-1604-xenial-v20210105 --image-project=ubuntu-os-cloud \
-    --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=k8s-20210107-$i
+    --tags=http-server,https-server --image=ubuntu-2004-focal-v20220712 --image-project=ubuntu-os-cloud \
+    --boot-disk-size=20GB --boot-disk-type=pd-standard --boot-disk-device-name=iisi-k8s-vm2-$i
 done
