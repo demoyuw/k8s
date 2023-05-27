b5673536ddaab187df1ef1da17e9f45d9601f942
demoyuw <demoyuw@gmail.com>
2023-05-27 12:06:18 +0800

    rancher image use stable tag

--- rancher/ubunu20_deploy_rancher.sh
+++ rancher/ubunu20_deploy_rancher.sh
@@ -7,7 +7,7 @@ touch $LOGFILE
 
 apt-get update -y &>> $LOGFILE
 apt-get install     apt-transport-https     ca-certificates     curl     software-properties-common  -y &>> $LOGFILE
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &>> $LOGFILE
 add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable" &>> $LOGFILE
@@ -17,4 +17,4 @@ usermod -aG docker demoyuw &>> $LOGFILE
 
 mkdir $WORK_DIR/rancher &>> $LOGFILE
 chmod 755 $WORK_DIR/rancher &>> $LOGFILE
-docker run -d --privileged --restart=unless-stopped -p 80:80 -p 443:443 -v $WORK_DIR/rancher:/var/lib/rancher rancher/rancher &>> $LOGFILE
+docker run -d --privileged --restart=unless-stopped -p 80:80 -p 443:443 -v $WORK_DIR/rancher:/var/lib/rancher rancher/rancher:stable &>> $LOGFILE
--- rancher/ubunu20_install_docker.sh
+++ rancher/ubunu20_install_docker.sh
@@ -7,7 +7,7 @@ touch $LOGFILE
 
 apt-get update -y &>> $LOGFILE
 apt-get install     apt-transport-https     ca-certificates     curl     software-properties-common -y &>> $LOGFILE
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &>> $LOGFILE
 add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable" &>> $LOGFILE
